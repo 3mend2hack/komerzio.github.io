245450da288998f2a2157b978cf9d2461983c750
KOMERZIO <90018054+3mend2hack@users.noreply.github.com>
2026-06-22 15:09:00 -0400

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,8 +1,7 @@
 // supabase-client.js
-const SUPABASE_URL = 'https://houfrgnlctliwkzzelmi.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhvdWZyZ25sY3RsaXdrenplbG1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2MTQ2OTMsImV4cCI6MjA5NjE5MDY5M30.zUfcA755LEjBbn-N05LrmwFqsOFITRP4qLzxjgPIy54';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const SUPABASE_URL = 'https://houfrgnlctliwkzzelmi.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhvdWZyZ25sY3RsaXdrenplbG1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2MTQ2OTMsImV4cCI6MjA5NjE5MDY5M30.zUfcA755LEjBbn-N05LrmwFqsOFITRP4qLzxjgPIy54'
 
-window.supabaseClient = supabase;
-export { supabase };
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
