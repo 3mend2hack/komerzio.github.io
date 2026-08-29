05b3e2d8e6760a5fe37b0f73aedddb01c563f8e8
KOMERZIO <90018054+3mend2hack@users.noreply.github.com>
2026-08-28 22:12:25 -0400

    Update supabase.js

--- mistreambot/supabase.js
+++ mistreambot/supabase.js
@@ -2,19 +2,12 @@
 // CONFIGURACIÓN DE SUPABASE
 // ============================================
 
-// URL de tu proyecto Supabase
 export const SUPABASE_URL = 'https://qddfdisbnwnnlvkrnckd.supabase.co';
-
-// ANON KEY (pública, se puede usar en el cliente)
 export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkZGZkaXNibndubmx2a3JuY2tkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY4NDQwMzcsImV4cCI6MjEwMjQyMDAzN30.9Jsgl0qepqjJ8oiiewyPZK3vOsqm49EnLCEtOar5MiQ';
 
-// SERVICE ROLE KEY (solo para operaciones del servidor, NO USAR EN CLIENTE)
 export const SUPABASE_SERVICE_ROLE = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkZGZkaXNibndubmx2a3JuY2tkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4Njg0NDAzNywiZXhwIjoyMTAyNDIwMDM3fQ.oLzwVITIFHhOaqxVZAGRDuBumAbpIO9Z8W2OPQ1JTb8';
 
-// ===== EMAIL AUTORIZADO =====
 export const AUTH_EMAIL = 'josuealexander.perez01@gmail.com';
-
-// ===== CLIENT ID DE GOOGLE (CORRECTO) =====
 export const GOOGLE_CLIENT_ID = '491062627132-q6a66kpblc45vfnt1fmni9592t0jqfnb.apps.googleusercontent.com';
-
+export const WORKER_URL = 'https://activar-poder.cubazon-oficial.workers.dev';
 export const BOT_VERSION = 'v3.0';
