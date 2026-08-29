9212663d91a65725d3820daa04d7ac5a36216673
KOMERZIO <90018054+3mend2hack@users.noreply.github.com>
2026-08-28 21:39:01 -0400

    Update supabase.js

--- mistreambot/supabase.js
+++ mistreambot/supabase.js
@@ -7,10 +7,7 @@ export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiO
 
 export const SUPABASE_SERVICE_ROLE = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkZGZkaXNibndubmx2a3JuY2tkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4Njg0NDAzNywiZXhwIjoyMTAyNDIwMDM3fQ.oLzwVITIFHhOaqxVZAGRDuBumAbpIO9Z8W2OPQ1JTb8';
 
-// ===== EMAIL AUTORIZADO =====
 export const AUTH_EMAIL = 'josuealexander.perez01@gmail.com';
-
-// ===== CLIENT ID DE GOOGLE =====
-export const GOOGLE_CLIENT_ID = '491062627132-q6a66kpblc45vfmt1fmni9592t0jqfnb.apps.googleusercontent.com';
+export const GOOGLE_CLIENT_ID = '491062627132-q6a66kpblc45vfnt1fmni9592t0jqfnb.apps.googleusercontent.com';
 
 export const BOT_VERSION = 'v3.0';
